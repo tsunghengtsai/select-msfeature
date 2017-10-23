b60170807b717d93d730d851f3ac13388705d1d2
Tsung-Heng Tsai <tsai.tsungheng@gmail.com>
2017-10-23 09:58:13 -0400

    Functions following feature selection

--- utilities.R
+++ utilities.R
@@ -359,7 +359,62 @@ extract_res <- function(fit) {
 }
 
 
-# Intermediate results during summarization
+# Modify bad-quality features & peaks
+modify_badftrpk <- function(df_all, df_rmftr = NULL, df_rmpk = NULL, cen_val = NA) {
+    
+    if (is.null(df_rmftr)) {
+        message("df_rmftr is missing - no information about features for removal")
+    }
+    if (is.null(df_rmpk)) {
+        message("df_rmpk is missing - no information about peaks for removal")
+    }
+    
+    df_all <- as_tibble(df_all) %>% 
+        modify_if(is.factor, as.character) %>% 
+        select(protein = PROTEIN, feature = FEATURE, label = LABEL, 
+               log2inty = ABUNDANCE, is_censored = censored, run = RUN, 
+               group = GROUP, subject = SUBJECT, orig_run = originalRUN)
+    
+    # Both df_rmftr & df_rmpk are missing - return the reformated data
+    if (is.null(df_rmftr) && is.null(df_rmpk)) {
+        df_all$log2inty[df_all$is_censored] <- cen_val
+        return(df_all)
+    }
+    
+    # Remove noisy features
+    if (!is.null(df_rmftr) && nrow(df_rmftr) > 0) {
+        df_rmftr <- df_rmftr %>% 
+            mutate(ftr_lab = str_c(feature_rm, label, sep = "_"))
+        df_sub <- df_all %>% 
+            mutate(ftr_lab = str_c(feature, label, sep = "_")) %>% 
+            filter(!(ftr_lab %in% df_rmftr$ftr_lab)) %>% 
+            select(-ftr_lab)
+    } else {
+        df_sub <- df_all
+    }
+    
+    # Handle outliers
+    if (!is.null(df_rmpk) && nrow(df_rmpk) > 0) {
+        df_rmpk <- df_rmpk %>% 
+            mutate(peak_lab = str_c(run, feature, label, sep = "_"))
+        df_sub <- df_sub %>% 
+            mutate(peak_lab = str_c(orig_run, feature, label, sep = "_"))
+        
+        # Censored the outliers of the endogenous (may need revisions...)
+        df_sub$is_censored[df_sub$label == "L" & df_sub$peak_lab %in% df_rmpk$peak_lab] <- TRUE
+        
+        # Replace outliers of the labeled with NA
+        df_sub$log2inty[df_sub$label == "H" & df_sub$peak_lab %in% df_rmpk$peak_lab] <- NA
+        
+        df_sub <- df_sub %>% select(-peak_lab)
+    }
+    df_sub$log2inty[df_sub$is_censored] <- cen_val
+    
+    return(df_sub)
+}
+
+
+# Generate subplot summarization & intermediate results (following modify_badftrpk)
 gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = TRUE) {
     
     if (is.null(censoredInt)) {
@@ -375,33 +430,33 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
     df_mss <- as_tibble(df_mss) %>% 
         modify_if(is.factor, as.character) %>% 
         mutate(
-            ftr_lab = paste(FEATURE, LABEL, sep = "_"), 
-            run_lab = paste(RUN, LABEL, sep = "_")
+            ftr_lab = paste(feature, label, sep = "_"), 
+            run_lab = paste(run, label, sep = "_")
         )
     
     # Is the dataset label-based (or label-free)?
-    is_labeled <- n_distinct(df_mss$LABEL) == 2
+    is_labeled <- n_distinct(df_mss$label) == 2
     
     # Replace censored values with censoredInt
     if (MBimpute) {
         if (censoredInt == "0") {
-            df_mss$ABUNDANCE[df_mss$censored] <- 0
+            df_mss$log2inty[df_mss$is_censored] <- 0
         } else if (censoredInt == "NA") {
-            df_mss$ABUNDANCE[df_mss$censored] <- NA
+            df_mss$log2inty[df_mss$is_censored] <- NA
         }
         df_mss <- df_mss %>% 
-            mutate(ind_obs = ifelse(censored, 0, 1))
+            mutate(ind_obs = ifelse(is_censored, 0, 1))
     }
     df_mss <- df_mss %>% 
-        mutate(is_missed = is.na(ABUNDANCE))
+        mutate(is_missed = is.na(log2inty))
     
     # Warning when a measurement is <= 0 but uncensored
-    if (df_mss %>% filter(ABUNDANCE <= 0, !censored) %>% nrow() > 0) {
-        message("There are ABUNDANCE values less than or equal to 0 but uncensored!")
+    if (df_mss %>% filter(log2inty <= 0, !is_censored) %>% nrow() > 0) {
+        message("There are abundance values less than or equal to 0 but uncensored!")
     }
     
     # Create a nested data frame wrt protein
-    nested <- df_mss %>% nest(-PROTEIN)
+    nested <- df_mss %>% nest(-protein)
     nb_prot <- nrow(nested)
     
     # Empty lists to save intermediate results (assuming MBimpute)
@@ -413,7 +468,7 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
     
     for (i in 1:nb_prot) {
         
-        str_prot <- paste(nested$PROTEIN[i], "(", i, "of", nb_prot, ")")
+        str_prot <- paste(nested$protein[i], "(", i, "of", nb_prot, ")")
         if (message) {
             message(paste("Getting the summarization by Tukey's median polish for protein", str_prot))
         }
@@ -421,7 +476,7 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
         oneprot <- nested$data[[i]]
         # Check if the protein has valid peaks for summarization
         valid_pks <- oneprot %>% 
-            filter(LABEL == "L", !is_missed, !censored)
+            filter(label == "L", !is_missed, !is_censored)
         if (nrow(valid_pks) == 0) {
             message(paste("Can't summarize for", str_prot, "because all measurements are NAs"))
             next()
@@ -429,7 +484,7 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
         
         # Remove features with only one measurement
         cnt_feature <- valid_pks %>% 
-            count(FEATURE)
+            count(feature)
         if (all(cnt_feature$n == 1)) {
             message(paste("Can't summarize for", str_prot, "because features have only one measurement across MS runs"))
             next()
@@ -437,54 +492,54 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
         # [TODO]: this would also remove labeled peptides - to be confirmed
         valid_ftrs <- cnt_feature %>% 
             filter(n > 1) %>% 
-            .$FEATURE
-        oneprot <- oneprot %>% filter(FEATURE %in% valid_ftrs)
+            .$feature
+        oneprot <- oneprot %>% filter(feature %in% valid_ftrs)
         
         # Remove uncovered runs (all endogenous are either missing or censored)
         covered <- valid_pks %>% 
-            filter(FEATURE %in% valid_ftrs) %>% 
-            distinct(RUN) %>% 
-            .$RUN
-        oneprot <- oneprot %>% filter(RUN %in% covered)
+            filter(feature %in% valid_ftrs) %>% 
+            distinct(run) %>% 
+            .$run
+        oneprot <- oneprot %>% filter(run %in% covered)
         
         # Impute censored values
         if (MBimpute) {
             if (is_labeled) {
-                oneprot_h <- oneprot %>% filter(LABEL == "H")
-                oneprot_l <- oneprot %>% filter(LABEL == "L")
+                oneprot_h <- oneprot %>% filter(label == "H")
+                oneprot_l <- oneprot %>% filter(label == "L")
             } else {
                 oneprot_l <- oneprot
             }
-            if (any(oneprot_l$censored)) {
+            if (any(oneprot_l$is_censored)) {
                 # Replace censored values by feature-specific thresholds
                 oneprot_l <- oneprot_l %>% 
                     group_by(ftr_lab) %>% 
-                    mutate(cen_thresh = 0.99 * min(ABUNDANCE[!(censored | is_missed)])) %>% 
+                    mutate(cen_thresh = 0.99 * min(log2inty[!(is_censored | is_missed)])) %>% 
                     ungroup() %>% 
-                    mutate(ABUNDANCE = ifelse(censored, cen_thresh, ABUNDANCE)) %>% 
+                    mutate(log2inty = ifelse(is_censored, cen_thresh, log2inty)) %>% 
                     select(-cen_thresh)
                 
                 # AFT model
                 valid_aft <- oneprot_l %>% filter(!is_missed)
-                insufDF <- nrow(valid_aft) < (n_distinct(valid_aft$FEATURE) + n_distinct(valid_aft$RUN) - 1)
+                insufDF <- nrow(valid_aft) < (n_distinct(valid_aft$feature) + n_distinct(valid_aft$run) - 1)
                 
                 set.seed(100)
-                if (n_distinct(valid_aft$FEATURE) == 1) {
-                    aftfit <- survreg(Surv(ABUNDANCE, ind_obs, type = "left") ~ RUN, data = oneprot_l, dist = "gaussian")
+                if (n_distinct(valid_aft$feature) == 1) {
+                    aftfit <- survreg(Surv(log2inty, ind_obs, type = "left") ~ run, data = oneprot_l, dist = "gaussian")
                 } else {
                     if (insufDF) {
-                        aftfit <- survreg(Surv(ABUNDANCE, ind_obs, type = "left") ~ RUN, data = oneprot_l, dist = "gaussian")
+                        aftfit <- survreg(Surv(log2inty, ind_obs, type = "left") ~ run, data = oneprot_l, dist = "gaussian")
                     } else {
-                        aftfit <- survreg(Surv(ABUNDANCE, ind_obs, type = "left") ~ FEATURE + RUN, data = oneprot_l, dist = "gaussian")
+                        aftfit <- survreg(Surv(log2inty, ind_obs, type = "left") ~ feature + run, data = oneprot_l, dist = "gaussian")
                     }
                 }
                 # Replace censored values with predicted values by AFT
                 oneprot_l <- oneprot_l %>% 
                     mutate(pred = predict(aftfit, newdata = oneprot_l, type = "response")) %>% 
-                    mutate(ABUNDANCE = ifelse(censored, pred, ABUNDANCE))
+                    mutate(log2inty = ifelse(is_censored, pred, log2inty))
                 
                 res_aft[[i]] <- oneprot_l %>%
-                    mutate(PROTEIN = nested$PROTEIN[i])
+                    mutate(protein = nested$protein[i])
                 
                 if (is_labeled) {
                     # Merge with labeled peptide
@@ -497,56 +552,56 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
             } else {
                 # No censored values
                 res_aft[[i]] <- oneprot_l %>%
-                    mutate(pred = NA, PROTEIN = nested$PROTEIN[i])
+                    mutate(pred = NA, protein = nested$protein[i])
                 oneprot$pred <- NA
             }
         }
         # Remove NA in abundance prior to TMP (likely introduced by AFT?)
-        oneprot <- oneprot %>% filter(!is.na(ABUNDANCE))
+        oneprot <- oneprot %>% filter(!is.na(log2inty))
         
         # Tukey's median polish
-        if (n_distinct(oneprot$FEATURE) > 1) { 
+        if (n_distinct(oneprot$feature) > 1) { 
             # More than 1 features are required for TMP
             if (!is_labeled) {
                 inty_wide <- oneprot %>% 
-                    select(FEATURE, RUN, ABUNDANCE) %>% 
-                    spread(FEATURE, ABUNDANCE)
+                    select(feature, run, log2inty) %>% 
+                    spread(feature, log2inty)
                 inty_mat <- data.matrix(inty_wide[, -1])
                 mp_out <- medpolish(inty_mat, na.rm = TRUE, trace.iter = FALSE)
                 
                 # Subplot result
-                res_subplot[[i]] <- data.frame(
-                    Protein = nested$PROTEIN[i], 
-                    LogIntensities = mp_out$overall + mp_out$row, 
-                    RUN = inty_wide$RUN
+                res_subplot[[i]] <- tibble(
+                    protein = nested$protein[i], 
+                    log2inty = mp_out$overall + mp_out$row, 
+                    run = inty_wide$run
                 )
             } else { 
                 # Labeled - additional adjustment based on the reference
                 inty_wide <- oneprot %>% 
-                    select(FEATURE, run_lab, ABUNDANCE) %>% 
-                    spread(FEATURE, ABUNDANCE)
+                    select(feature, run_lab, log2inty) %>% 
+                    spread(feature, log2inty)
                 inty_mat <- data.matrix(inty_wide[, -1])
                 mp_out <- medpolish(inty_mat, na.rm = TRUE, trace.iter = FALSE)
                 
-                df_labeled <- data.frame(
-                    Protein = nested$PROTEIN[i], 
-                    LogIntensities = mp_out$overall + mp_out$row, 
+                df_labeled <- tibble(
+                    protein = nested$protein[i], 
+                    log2inty = mp_out$overall + mp_out$row, 
                     run_lab = inty_wide$run_lab
                 ) %>% 
                     mutate(
-                        RUN = str_sub(run_lab, 1, -3), 
-                        LABEL = str_sub(run_lab, -1, -1)
+                        run = str_sub(run_lab, 1, -3), 
+                        label = str_sub(run_lab, -1, -1)
                     ) %>% 
                     select(-run_lab)
                 
                 # Adjustment wrt the labeled peptides
                 adj_h <- df_labeled %>% 
-                    filter(LABEL == "H") %>% 
-                    mutate(log2inty_adj = median(LogIntensities, na.rm = TRUE) - LogIntensities) %>% 
-                    select(RUN, log2inty_adj)
+                    filter(label == "H") %>% 
+                    mutate(log2inty_adj = median(log2inty, na.rm = TRUE) - log2inty) %>% 
+                    select(run, log2inty_adj)
                 df_adjlabeled <- df_labeled %>% 
                     left_join(adj_h) %>% 
-                    mutate(LogIntensities = ifelse(!is.na(log2inty_adj), LogIntensities + log2inty_adj, LogIntensities)) %>% 
+                    mutate(log2inty = ifelse(!is.na(log2inty_adj), log2inty + log2inty_adj, log2inty)) %>% 
                     select(-log2inty_adj)
                 
                 res_labeled[[i]] <- df_labeled
@@ -554,29 +609,29 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
                 
                 # Adjusted subplot results for the endogenous
                 res_subplot[[i]] <- df_adjlabeled %>% 
-                    filter(LABEL == "L") %>% 
-                    select(-LABEL)
+                    filter(label == "L") %>% 
+                    select(-label)
             }
         } else { 
             # single feature 
             if (!is_labeled) {
                 res_subplot[[i]] <- oneprot %>% 
-                    filter(!is.na(ABUNDANCE)) %>% 
-                    mutate(Protein = nested$PROTEIN[i]) %>% 
-                    select(Protein, LogIntensities = ABUNDANCE, RUN)
+                    filter(!is.na(log2inty)) %>% 
+                    mutate(protein = nested$protein[i]) %>% 
+                    select(protein, log2inty, run)
             } else {
                 # Labeled - additional adjustment
                 df_labeled <- oneprot %>% 
-                    filter(!is.na(ABUNDANCE)) %>% 
-                    mutate(Protein = nested$PROTEIN[i]) %>% 
-                    select(Protein, LogIntensities = ABUNDANCE, RUN, LABEL)
+                    filter(!is.na(log2inty)) %>% 
+                    mutate(protein = nested$protein[i]) %>% 
+                    select(protein, log2inty, run, label)
                 adj_h <- df_labeled %>% 
-                    filter(LABEL == "H") %>% 
-                    mutate(log2inty_adj = median(LogIntensities, na.rm = TRUE) - LogIntensities) %>% 
-                    select(RUN, log2inty_adj)
+                    filter(label == "H") %>% 
+                    mutate(log2inty_adj = median(log2inty, na.rm = TRUE) - log2inty) %>% 
+                    select(run, log2inty_adj)
                 df_adjlabeled <- df_labeled %>% 
                     left_join(adj_h) %>% 
-                    mutate(LogIntensities = ifelse(!is.na(log2inty_adj), LogIntensities + log2inty_adj, LogIntensities)) %>% 
+                    mutate(log2inty = ifelse(!is.na(log2inty_adj), log2inty + log2inty_adj, log2inty)) %>% 
                     select(-log2inty_adj)
                 
                 res_labeled[[i]] <- df_labeled
@@ -584,8 +639,8 @@ gen_subplot <- function(df_mss, censoredInt = NULL, MBimpute = TRUE, message = T
                 
                 # Adjusted subplot results for the endogenous
                 res_subplot[[i]] <- df_adjlabeled %>% 
-                    filter(LABEL == "L") %>% 
-                    select(-LABEL)
+                    filter(label == "L") %>% 
+                    select(-label)
             }
         }
     }
